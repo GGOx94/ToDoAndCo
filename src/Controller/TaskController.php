2ed365a9507cfa7e0e332cc36bfe98ce78bad7f3
GGOx94 <mustbehax@hotmail.fr>
2023-04-02 23:10:56 +0200

    Users tasks rights - Fixed user tasks security - Changed task list route (todo tasks) - Updated security.yaml accordingly

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
@@ -19,7 +20,7 @@ class TaskController extends AbstractController
     ) {
     }
 
-    #[Route('/tasks', name: 'task_list')]
+    #[Route('/tasks/todo', name: 'task_list_todo')]
     public function listTasks(): Response
     {
         return $this->render('task/list.html.twig', [
@@ -53,7 +54,7 @@ class TaskController extends AbstractController
 
             $this->addFlash('success', 'Votre tâche a bien été ajoutée.');
 
-            return $this->redirectToRoute('task_list');
+            return $this->redirectToRoute('task_list_todo');
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
@@ -62,6 +63,8 @@ class TaskController extends AbstractController
     #[Route('/tasks/{id}/edit', name: 'task_edit')]
     public function editTask(Task $task, Request $request): Response
     {
+        $this->checkUserRightsOnTask($task);
+
         $form = $this->createForm(TaskType::class, $task);
 
         $form->handleRequest($request);
@@ -71,7 +74,7 @@ class TaskController extends AbstractController
 
             $this->addFlash('success', 'La tâche a bien été modifiée.');
 
-            return $this->redirectToRoute('task_list');
+            return $this->redirectToRoute('task_list_todo');
         }
 
         return $this->render('task/edit.html.twig', [
@@ -83,6 +86,8 @@ class TaskController extends AbstractController
     #[Route('/tasks/{id}/toggle', name: 'task_toggle')]
     public function toggleTask(Task $task): Response
     {
+        $this->checkUserRightsOnTask($task);
+
         $task->toggle(!$task->isDone());
         $this->em->flush();
 
@@ -91,17 +96,31 @@ class TaskController extends AbstractController
                 $task->getTitle(), $task->isDone() ? 'faite' : 'non terminée')
         );
 
-        return $this->redirectToRoute('task_list');
+        return $this->redirectToRoute($task->isDone() ? 'task_list_todo' : 'task_list_done');
     }
 
     #[Route('/tasks/{id}/delete', name: 'task_delete')]
     public function deleteTask(Task $task): Response
     {
+        $this->checkUserRightsOnTask($task);
+
         $this->em->remove($task);
         $this->em->flush();
 
         $this->addFlash('success', 'La tâche a bien été supprimée.');
 
-        return $this->redirectToRoute('task_list');
+        return $this->redirectToRoute('task_list_todo');
+    }
+
+    private function checkUserRightsOnTask(Task $task): void
+    {
+        if ($task->getUser()->getUsername() === "anonyme") {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+            return;
+        }
+
+        if ($task->getUser() !== $this->getUser()) {
+            throw new AccessDeniedHttpException();
+        }
     }
 }
